9561365183153eea395e6192a7e2a037e1967c81
M. Sathis Kumar <73054487+mskumarsk@users.noreply.github.com>
2022-09-06 21:56:31 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,18 +3,32 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
 	// write your code here
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the array size");
+        int size = scan.nextInt();
+        int arr[] = new int[size];
+        int i = 0, j = 0, k = 0;
+        System.out.println("Enter the elements to the array");
+        for (i = 0; i < size; i++) {
+            arr[i] = scan.nextInt();
+        }
+        for (i = 0; i < size; i++) {
+            for (j = i + 1; j < size; j++) {
+                if ((arr[i] ^ arr[j]) == 0) {
+                    for (k = j; k < size - 1; k++) {
+                        arr[k] = arr[k + 1];
+                    }
+                    j--;
+                    size--;
+                }
+            }
+        }
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the first number: ");
-        int a = scanner.nextInt();
-        System.out.print("Enter the second number: ");
-        int b = scanner.nextInt();
-        a = a ^ b;
-        b = a ^ b;
-        a = a ^ b;
-        System.out.print("Swapping :"+" "+"a = " +a +", b = " +b);
+        System.out.println("The new array");
+        for (i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
